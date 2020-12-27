1a1dc3c71e52cc601116f5e27d657936f516845e
nobumisu-1995 <masugata1995@mailcom>
2020-12-27 23:08:38 +0900

    edit app

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  def result
+    if postal_code = params[:postal_code]
+      params = URI.encode_www_form({zipcode: postal_code})
+      uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?#{params}")
+      response = Net::HTTP.get_response(uri)
+      result = JSON.parse(response.body)
+      if result["results"]
+        @zipcode = result["results"][0]["zipcode"]
+        @address1 = result["results"][0]["address1"]
+        @address2 = result["results"][0]["address2"]
+        @address3 = result["results"][0]["address3"]
+      else
+        flash[:notice] = "郵便番号は存在しません。"
+      end
+    else
+      flash[:notice] = "郵便番号を入力してください。"
+    end
+  end
+end
